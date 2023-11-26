18551b98982c8b09bb57632bf419dde281ebb79b
ndnastia <ndnastiadatsiuk@gmail.com>
2023-11-26 20:01:48 +0200

    HW3

--- src/1.ts
+++ src/1.ts
@@ -4,15 +4,11 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
 
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
+class Student{
+
+  constructor( public name: string, public age: number, public grade: string) {
+    
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,53 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature():number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() :Key {
+        return this.key;
+    }
+}
+
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor(key:Key) :void;
+
+    comeIn (person: Person) {
+        if(this.door) {
+            this.tenants.push(person);
+        }
+    }
+    
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(key.getSignature === this.key.getSignature) {
+            this.door === true
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
